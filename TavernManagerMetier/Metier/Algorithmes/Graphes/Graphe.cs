88914016ccf69860eb99270d9707088d70744a43
MerlinChapelle <104969837+MerlinChapelle@users.noreply.github.com>
2023-05-15 16:48:55 +0200

    y'a les amis

--- TavernManagerMetier/Metier/Algorithmes/Graphes/Graphe.cs
+++ TavernManagerMetier/Metier/Algorithmes/Graphes/Graphe.cs
@@ -12,15 +12,21 @@ namespace TavernManagerMetier.Metier.Algorithmes.Graphes
         private Dictionary<Sommet,int> couleurs; 
         public List<Sommet> Sommets => this.sommets.Values.Distinct().ToList<Sommet>();
 
-        private void AjouterSommet(Client client)
-        {
-            Sommet sommet = new Sommet();
-            sommets.Add(client, sommet);
-        }
+        
 
         private void ChangerCouleur(Sommet sommet,int couleur) 
         {
             couleurs.Add(sommet, couleur);
+        }
+        private void AjouterSommet(Client client,Sommet sommet)
+        {
+           if(!this.sommets.ContainsKey(client)) 
+            {
+                this.sommets[client] = sommet;
+                sommet.NbClients ++ ;
+                foreach(Client ami in client.Amis) this.AjouterSommet(ami, sommet);
+            }
+           
         }
 
         private void AjouterArette(Client client1, Client client2)
@@ -31,8 +37,8 @@ namespace TavernManagerMetier.Metier.Algorithmes.Graphes
         public Graphe(Taverne taverne)
         {
             sommets = new Dictionary<Client, Sommet>();
-            foreach(Client client in taverne.Clients) { sommets[client] = new Sommet(); }
-            foreach(Client client2 in taverne.Clients) { foreach (Client ennemie in client2.Ennemis){ sommets[client2].AjouterVoisin(sommets[ennemie]);  } }
+            foreach(Client client in taverne.Clients) { AjouterSommet(client, new Sommet()); }
+            foreach(Client client2 in taverne.Clients) { foreach (Client ennemie in client2.Ennemis) { sommets[client2].AjouterVoisin(sommets[ennemie]);  } }
             foreach (Sommet sommet in Sommets) { ChangerCouleur(sommet, 0); }
         }
     }
--- TavernManagerMetier/Metier/Algorithmes/Graphes/Sommet.cs
+++ TavernManagerMetier/Metier/Algorithmes/Graphes/Sommet.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 namespace TavernManagerMetier.Metier.Algorithmes.Graphes
 {
     internal class Sommet
-    { private List<Sommet> voisins;
-      
+    {
+        private List<Sommet> voisins;
+        private int nbClients;
+        public int NbClients { get { return nbClients; } set { nbClients=value; } }
+
         public List<Sommet> Voisins { get { return voisins; } }
 
 
         public Sommet() 
         {
             voisins = new List<Sommet>();
+            nbClients = 0;
         }
 
         public void AjouterVoisin(Sommet sommet)
