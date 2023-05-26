2f53381355a5cea5f9072e7b19a9c2d25f6b2650
iutdijonMalindi <Valentin_Malindi@etu.u-bourgogne.fr>
2023-05-26 15:25:56 +0200

    Ajout de l'algo WelshPowell

--- TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeLDO.cs
+++ TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeLDO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Valentin Malindi
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
--- TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeWelshPowell.cs
+++ TavernManagerMetier/Metier/Algorithmes/Realisations/AlgorithmeWelshPowell.cs
@@ -11,19 +11,46 @@ namespace TavernManagerMetier.Metier.Algorithmes.Realisations
 {
     public class AlgorithmeWelshPowell: IAlgorithme
     {
-        public string Nom => "Chacun sa table";
+        public string Nom => "WelshPowell";
 
         private long tempsExecution;
         public long TempsExecution => -1;
 
         public void Executer(Taverne taverne)
-        {  Stopwatch stopwatch = Stopwatch.StartNew();
-           Graphe graphe = new Graphe(taverne);
-            for (int i=0;i<taverne.Clients.Count();i++)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            taverne.Clients.OrderByDescending(s => s.Ennemis.Count);
+            Graphe graphe = new Graphe(taverne);
+            int c = 1; 
+            int j = 0;
+            bool voisincolorie = false;
+            while (graphe.Couleurs.Values.Contains(0) ) 
             {
-                
                 taverne.AjouterTable();
-                taverne.AjouterClientTable(i, i) ;
+
+                 
+                for (int i = 0; i < graphe.Sommets.Count(); i++)
+                {
+                    j = 0;
+                    voisincolorie = false;
+                    
+                    while ( (voisincolorie == false) && ( j < graphe.Sommets[i].Voisins.Count() ) )
+                    {
+                        if (graphe.Couleurs[graphe.Sommets[i].Voisins[j]] == c)
+                        {
+                            voisincolorie = true;
+                        }
+                        j++;
+                    }
+                    
+                    if (voisincolorie == false)
+                    {
+                        graphe.ChangerCouleur(graphe.Sommets[i], c);
+                        taverne.AjouterClientTable(i, c - 1);
+                    }
+                    
+                }
+                c++;
             }
             stopwatch.Stop();
             this.tempsExecution = stopwatch.ElapsedMilliseconds;
